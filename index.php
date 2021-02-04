04ce07437966a75fa076459530965ae870f208d2
ksovrela0 <gioza-gioza@mail.ru>
2021-02-04 18:13:49 +0400

    fixed a lot of things

--- index.php
+++ index.php
@@ -150,23 +150,23 @@
       <div class="page">
          <!-- Sidemenu --> 
          <div class="main-sidebar main-sidebar-sticky side-menu ps">
-            <div class="sidemenu-logo"> <a class="main-logo" href="index.html"> <img src="assets/img/brand/logo.png" class="header-brand-img desktop-logo" alt="logo"> <img src="assets/img/brand/icon.png" class="header-brand-img icon-logo" alt="logo"> <img src="assets/img/brand/logo-light.png" class="header-brand-img desktop-logo theme-logo" alt="logo"> <img src="assets/img/brand/icon-light.png" class="header-brand-img icon-logo theme-logo" alt="logo"> </a> </div>
+            <div class="sidemenu-logo"> <a class="main-logo" href="index.php"> <img src="assets/img/brand/logo.png" class="header-brand-img desktop-logo" alt="logo"> <img src="assets/img/brand/icon.png" class="header-brand-img icon-logo" alt="logo"> <img src="assets/img/brand/logo-light.png" class="header-brand-img desktop-logo theme-logo" alt="logo"> <img src="assets/img/brand/icon-light.png" class="header-brand-img icon-logo theme-logo" alt="logo"> </a> </div>
             <div class="main-sidebar-body">
                <ul class="nav">
                   <li class="nav-label">სტატისტიკა</li>
-                  <li class="nav-item active"> <a class="nav-link" href="index.html"><i class="fe fe-airplay"></i><span class="sidemenu-label">მთავარი/სტატისტიკა</span></a> </li>
+                  <li class="nav-item active"> <a class="nav-link" href="index.php"><i class="fe fe-airplay"></i><span class="sidemenu-label">მთავარი/სტატისტიკა</span></a> </li>
                   <li class="nav-label">შეკვეთები</li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-database"></i><span class="sidemenu-label">მიმდინარე შეკვეთები</span></a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-database"></i><span class="sidemenu-label">შეკვეთების ისტორია</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-database"></i><span class="sidemenu-label">მიმდინარე შეკვეთები</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-database"></i><span class="sidemenu-label">შეკვეთების ისტორია</span></a> </li>
                   <li class="nav-label">ობიექტი</li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-database"></i><span class="sidemenu-label">კატალოგი</span></a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-layers"></i><span class="sidemenu-label">პროდუქციის კატეგორიები</span></a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-aperture"></i><span class="sidemenu-label">ბანერი</span></a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-shopping-cart"></i><span class="sidemenu-label">სპეციალური შეთავაზებები</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="products.php"><i class="fe fe-database"></i><span class="sidemenu-label">კატალოგი</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-layers"></i><span class="sidemenu-label">პროდუქციის კატეგორიები</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-aperture"></i><span class="sidemenu-label">ბანერი</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-shopping-cart"></i><span class="sidemenu-label">სპეციალური შეთავაზებები</span></a> </li>
                   
                   <li class="nav-label">ფინანსები</li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-database"></i><span class="sidemenu-label">ფინანსური სტატისტიკა</span></a> </li>
-                  <li class="nav-item"> <a class="nav-link" href="widgets.html"><i class="fe fe-database"></i><span class="sidemenu-label">ფინანსების მართვა</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-database"></i><span class="sidemenu-label">ფინანსური სტატისტიკა</span></a> </li>
+                  <li class="nav-item"> <a class="nav-link" href="#"><i class="fe fe-database"></i><span class="sidemenu-label">ფინანსების მართვა</span></a> </li>
 
                   
                </ul>
