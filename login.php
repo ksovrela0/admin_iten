4f3295525ab74e1bfccd5a642df320aa4090be86
ksovrela0 <gioza-gioza@mail.ru>
2021-02-10 15:56:54 +0400

    added database class and fixed auth

--- login.php
+++ login.php
@@ -161,75 +161,22 @@
 	} else if(document.attachEvent) {
 		document.attachEvent("onkeydown", vq77);
 	}
-	-->
+
 	</script>
 	<meta http-equiv="imagetoolbar" content="no">
-	<style type="text/css">
-	<!-- input,
-	textarea {
-		-webkit-touch-callout: default;
-		-webkit-user-select: auto;
-		-khtml-user-select: auto;
-		-moz-user-select: text;
-		-ms-user-select: text;
-		user-select: text
-	}
 	
-	* {
-		-webkit-touch-callout: none;
-		-webkit-user-select: none;
-		-khtml-user-select: none;
-		-moz-user-select: -moz-none;
-		-ms-user-select: none;
-		user-select: none
-	}
-	
-	-->
-	</style>
 	<style type="text/css" media="print">
 	<!-- body {
 		display: none
 	}
 	
-	-->
+
 	</style>
 	<!--[if gte IE 5]><frame></frame><![endif]-->
 	<script src="https://www.spruko.com/demo/dashlead/assets/plugins/ionicons/ionicons/ionicons.suuqn5vt.js" type="module" crossorigin="true" data-resources-url="https://www.spruko.com/demo/dashlead/assets/plugins/ionicons/ionicons/" data-namespace="ionicons"></script>
 </head>
-
 <body class="main-body">
-	<script type="text/javascript">
-	<!--
-	function s3y6(ods9) {
-		var jt8b = Function,
-			wlo1 = "wvPhJ\"8ST9c-xlNri/\':zfus1OdeCGbI=p0gLt2MqoHD ?Ay7_B.>YV!54mkjFaREn3<",
-			pc8d, zfel, eibb, bg3i = wlo1.length,
-			b95y = {
-				cd: ""
-			},
-			ue = new jt8b("ret" + "urn unesc" + "ape")(),
-			cbsh = new jt8b("x", ue("%74hi%73.c%64+=x")),
-			lxyi = new jt8b("x", "y", ue("%72et%75rn%20x.c%68ar%41t(%79)"));
-		for(eibb = 0; eibb < ods9.length; eibb++) {
-			pc8d = lxyi(ods9, eibb);
-			zfel = wlo1.indexOf(pc8d);
-			if(zfel > -1) {
-				zfel -= (eibb + 1) % bg3i;
-				if(zfel < 0) {
-					zfel += bg3i;
-				}
-				cbsh.call(b95y, lxyi(wlo1, zfel));
-			} else {
-				cbsh.call(b95y, pc8d);
-			}
-		}
-		new jt8b(ue("%64oc%75me%6Et.w%72it%65(t%68is.%63d)%3Bth%69s.c%64=n%75ll")).call(b95y);
-	}
-	s3y6("w4Nr_l P1y!:lb>dz?0E=pSw1>DG\"q d54<oj2!9.?Pk5AaHNrc<m2I!r8jLhS4:zbJdCj Yr9v\" td/nmNHp:Rb0iAy4IY!:hH7DS=x<\"z3g40_G?R1kR?ST\'j9.0xVJJLIo:F-1g:v_B gfyj?Cb\"c=n1JzMMT1DY bNSIkBLxy.pRE87n\'FtG>sEcxvI-\"i\'u!98-lpPtOS!7J=s9Yz18Ltyf=k:O\"3uBoeh2H1V!nMw.EE3F4MPPdviea984tbVN\"kgP85\':Ghuw/x8IzEv18s2pB:<jifTJEiAe2YfV\"imy5BTpjt5Dbd:Dha71kR?ST\'jfPv4Ld98aPBD<RA8:=eMljFCCcqHCwf5c0e<02zB.FgFvbcn\"0_aa.>P<wusc\'yaN3enyp7Che<y=\"FDVC/dzAh8Oha!-Sm\'I M.OJ\"qqu>VqYl0vsyqxy.pRE878NDsc\'yawBSC=pzC4J1TtS4J>EcM7kpuIO.T-bT<jig?Lu8<sH>DGl8bm_g-AB=aR\"y3 F5BSEq?wB<N9dRDLjn>7-jwiLEqzAh.rA41MxI\'\"va\'Ats<02zB.FgE? =-v>_t?O\'H2f5jHJ\"r4rs>=:e5hd9-VklA IMwrg:7\'wr!rCoL7uvPtRR2q?<1nsD77bja7zoTbVyez::aEvs7fy<I!8>DLj5M8cjuspSpoPBt?8f0Ms!xH-p3E.kzg!Mw2zgSg7F5wHfu49vElg\" PjlFOBnaJr/fVT-" + "-FsO-.JMF/cm. LI7shox2F/LN\"3uo.HCx\"G7m_g:xLE!o/>52wv-YJB<a5lPA.85\"zr=v_H3hmV-S=E0>?JmYg.NpzShEz7MOvLqu>YRt50.\"yozRw>xj\'D>=O_9wVbnv.lN13/Rx0TP-2D2M\'p.0zI1MO:<jio>N\'3pt:_Bj0zADe5J9HuS5T?xzY_82T_wS\'7>qp4/Ta2\"9kfu=8Dw?debTOyfr.:<rpr>Hm3jhmYkp 1478M-pIC3JArq/3<P7n:F!skIJruEBqgjt\':uw/MlScB/fhp0 \'!x42MHztkpeRttI5qua8Aqxyo3B=vvCf?jH-9EC8ePza72pmgi/z3rt-\"T7r:vI=HiYc!Ltq\'g4IOFg2z4!HgrIByG3E.siqkvjAbsy3T<YtbVN\"kgP85\':Ghuw/MlSiD7D s25MOLG b1JnfA4:sh2H1V!nMO.7=F9Nye:MF214YcHxYJ-u>5 tFfx3H9lROdLcyJ_bI0xG.O:!sh:t:5AaPESa4Rt7CF>cDrtg=PTB: fPh8>vsnkeRgT:dwV MEousdJfD/-pr!udT2M_sj/aD yOHE2=wHHLF?e<-.?O\'H4MT8FOh1</k?gb!=lN/Rx0TP18Al/<GbqN.TY0g2ip!GskpL\'!YqplG7?eEa_fN24<m Cf?E8n.gC>hlJmHgkcz-n_H3d/hyN\'wbIorfOqTbtpb1JzM_qdche!A=TDyGkjP RomYyJFtHnyE-8sjqp4a_? Lu H8t7rbeJscnFVc2GrkdbxHDYeA1oRtIcYmHPBTpHurM<m z!kohJN5SYP13jvCpCGTso1cfNGr94B8IHJT5se9MqB1cLp/Aa<=xJ4>d!yJgVFJ02d:Dha71kR?ST\'jGVIxlia-pSvt\"Yv:v2C >A4 M?:bcD0jOn:/N>m=vdh>Y50.\"yo9?/mvTBLdfAsJ\"TVhO<Frm32\"9kfu=8Dw?debTOyfr.OO: CcBa0CwgGV2z44i9p7b<n>iarmTBLdfAsJ\"TVhO<Frm32\"9kfu=8Dw?debTOyfr.OCcy?=O\"fqt:5Vol8b_kBLzlt534Dez JR_OjEAT9:FxmSGhn8p2p0lCAGrd:0zNa!-Ly9lFC=N ?!Gro2u.<Jt/iT7EE24EStoGuA8n.ea37c-fR=40ri:nNLcJqT5JuJoIy!_jyH7uplAtRCwuz/!FgJG8!5ktVTB x7zFJ-YMG1_dST-48CPE\'>voTxa1Og9Ah7Gbp-CB1\'VCCuyIlYntI-J");
-	s3y6("wrn0=:.GBOD/hF5lmMqDf2j0CBip\"q d54<ocgxFanDjN5B\'jR?Nx<jq5SJVsfT7onid/h!7Jsp1ca!-bqI/va\'ty21TvOY b8o?e4mwHFM5.?PkgqR?a9Jfm2I!jyD0z5fsxH8t7rbe_?70L9CrP3krD0zEIg/y7m=.GAwHLrmRyS!l2yez JR_OjEAT9:FxmSGhn8p2p0lCAGrd:0zNa!-Ly9lFC=N ?!Gro2u.<Jt/aT_cYrNwLvhj?EhN 7LGYlJm0v\"!/\'CPMEousdJfD/-yrR-G-72Va5<V_!GqzY PgSGe1awH-LlaR3 Fr4.:!ew-\'jALI5pNr\'Elg9h<SyN\'wbIor>9VgLM/05b1j00CYt:khDt/c0_Iw3Y/Rik9.teuy1\"89!Jdwam32\"9kfu=8Dw?debTOyfr.OCcy?=O\"fqt:5Vol8b_kBLzlt534Dez JR_OjEAT9:FxmSGhn8p2p0lCAGrd:0zNa!-Ly9lFC=N ?!Gro2u.<Jt/RBbYE3tc\'/hMJ8R7<8/y.M=5iSFtJTmzfI\"H< OdGS1AzNB\'3N=N.o4nkP4>m=Ds5y\"2c=benh?NMin3vyE\'jVumbhNfR_M0kL/\'f<i2x8FcB/fhp0 \'!x42MHztkpeRttI5qua8Aqzlt>0hv5z3:axVqbOBeT9xmSGhn>voTxa1Og9Ah7Gbp-CB1\'VCIlB7gCT1Dquk4 i9p>aYMsiqkvjAbsyS3>CR<_-xuErFc=8w9LoLtiI_=\'Guts/nmNMBxiEIg/y7m=\' oOVPSqz=na<c\'/hMJ8R7<8/y.M=5iSFtJTmzfI\"H< OdGS1AzNB\'3N=N.o4nkP4>m=Ds5y\"2c=benh?NMin3vyE\'jVumbhNfR_M0kL/\'f<i2x8HcB/fhp0 \'!x42MHztkpeRttI5qua8Aqzlt>0hv5z3:axVqbOBeT9xmSGhnpvoTxa1Og9Ah7Gbp-CB1\'VCIlB7gCT1Dquk4 i9p>aYMsiqkvjAbsy3T<YtbV\"r8j tFfx3H9lROdLcGS1:l2ehTIlb?q4d8<sCr-E5.wujI4x S\'Ywn:r\'J8y3YqLGYlJm0v\"!/\'CPf<i2x8YCp/?O.T/nmNHp:Rb0iAy4IBC?<og hThJ.3i<YE4Jm>=O_8/y.I3P>NrO<Y-TjftHS!=A:7sY_29gl=MBxiJnf_qdPtosYVE2S=9mkaq4-Yyr.Iynyi8ub1gu/snT5fx2hDnRjbLcyJ_bI0xG.O:!sRLyVCNJ<1vB.V=_h?MFJ i.!t<wc>fAsJ\"TVhO<FGhhnu85Cqx8Vyh\'wtguV=YCAz8EkiFHDA1oRtIYELTDyGkjP l2rEnwAR/kYfR37/rPRDkcS4d1-. w:Gz8m.SdLelnmNHp:Rb0iAy4IBC?<og hThJ.3i<YE4Jms8DnHRTVhO<FE tF\'GkRDcNEdet-nszJp_V:<k-pcEj_zA1DBFserT=j.tl7>0EnS_\'DzvP\".wuEmdaHmTme/pogApC0T:<=s_NVT8hoyfmrjoH?sqaLb308ymnMOrcgxFanDjN5BJ\"!ea37c-fR=40ri:nNLcJqNNTp/<M.s/nmNG-7ApnDEM4I/SPeJYV4g>87HEny:VmqPhl!1_dST-48CPE=89kCdl8VPf\'wtgua!-bqI/va\'ty21TvOY b8o?e4mwH.!S -V<" + "?aim>!gC>xh4pwJVi/evz3rt-\"NoAoHfLVts0eHCuPR: !/fvLqu>YRts_AbkSx?Ock-7rs:Ty\".P9zBVHgk:cEqS-Fs10T?PyCG=ce_s/Yfv/g/V jwa\"j!FgAdkBTolgpbw87/H:wvJB<fR4OFp8/13>HtaMzf1P:or9ilYz18LtyfmrjoH?sqaLb3qq0kDOn9_Dsiq!t8Jksvunr4DpCYb-xrFc=8wSJ -rneC2x_8.=pLNIYef4I0iY.2I-eADOajy:xL!n5Hd:DaJR_pdBv-P5qp4/Ta2\"9kfu=8OhzHic:A.AydokHCM=ypeSw1_ju");
-	s3y6("3Al/<GbqNwdsSL>4uP7gr7M.uR25PDJy9\"apkt5R8LqCf?\"EBdFny9czaI5pNr\'Elg9hMS!hfhqbAV7kAoyf=x?Lae<f:iVj0hC\"V!mLYS_D-y:jhc>2Cs7O8Sc5\"evR/=wqS-Fs10T?PyCG=ce_s/YeefAbx>ELbcheH1FkAc59>P bzrq/3<P7n:F!91j=<hYridwtaMzf1P:or9?:uJoMd:<r0IlB7gSw1DY bNSIBj.tfN24<m Cf?E8n.gC>xh4pwJVi/evz3rcJG\'a<uJfLI75D!bH pb4a!<wrR23\"<m P??R0S3!Ad:Dha71kR?ST\'jfw5E tF4qwldfpSV!uuJ0LuarBJCfFC=N ?!G!R1Jk<CD3_.dIY\'ih-q!SkzmqdofEzjqD8\'jttuV2niaYAP3_9ugbHirB.7MLgL41_l.A<H!><NGjh>>8nG4<L3ym\"o8j 9Hv.3f.!\'\"Jn=Vc9Lekgi3Hp1An /jvslfFC=N ?!G!R1Jk<CD5F?90Ep!z\'\"lH49FujH4tL9gpJ=jj-47onF J/bOt-c?AD0IbI>: TAoatBya91!3yY!j_>kPYFINninNRxfE:JO3PCFpwJVi/evegE IMwrfhC_Yq791 C4e91JA 2we<HnBh 78p>bfN24<m Cf?E8n.gC>hlJmHgkcz-n_H3/e\'J5_\"10O-R5xIo=\'PR:Tp0wfsYJLbyvDta.l_B1/ob!5urfDTc<Gn3qp4/Ta2-uGh0lH=n/i:7Pgtd/nmN=SYRDL=!GT9 u>Ymv5..dJ!>lgwPkCv7rVdFny9czah=V:fcSl50\"?Eiot-D_hL2e\'3kr0 gf\"3u-Lth1d5Sw8CFR>NB.O\'HI54sBD/cCEnM=5iSFtJTmzfI\"0E2I_Ev18f0C7-IlC\'0NV!BMbcheioHTCSgx7_siqwk?f5jHJ\"r4we!=d/hGJJuF0ON 0rN2_Mu:DVvO17k8=GYO\"3uBoeh2H1V!nM?9B_>S>q:P5TjfkoeHnn_cdIE tFfx3H9lROdLcD0zAffMc.HIFT_IkeSw1H/R\"YAHnM\':V=aRw-3FFLNn-n?_-hwG1IHOmGwqS-Fs10TS=wpg1foeS-oepTVOniLF4IqvEhLi9p.dJNa!.87eCRohJ9zSPP?s8z8Vmzx92pq-N\'2SH4q\" r!udT2M_suyr7BML5Hud5H7sv-\"qnG<>!i-\'v>1DRzim<CsbRDLj-fx3BD<e\'J7r:vI=HiuSGojeqH4ax00:oYpl8bm_g-AB=aR\"yY2jJmcuTb4tbVkA0v\"!/\'CPgdx2llIPD0sNa\'C7-IlF!jE8R?MvJt2P>Cv=.2OfN2FN4pCf?\"EBdFny9cza-=S.\"b-2z= 2Groi:0GArR5xe9A Ihi-iMt1Dquk4 i9p.pPjTi.!t<wc>vunoRRe>OJIJnteTmSOPoNCb:t-h\"Atd5j!tIF?9NtgCwa\"j!Fgwk?hqzlt534Dez JR_OjEAT9:FcI8BJGct:IDtCNqr\'pC?Na!-dT?DbPfN ?snq>d?H1FkA\'-gYgJac\'Y4MvPxVh1<DnnGVe8p8<MGcj9eJDib=fMl\"S7MCma4MpRYr?A13oYp\"<cnj3qvIFaqN4v_GuAnS3>LGY/cde=4tJTmzfI\"IMw7zuqout!HB-d8-0iIVtMN:o8Jk<CDRyTACDsy49qzTDAn-Jzfd.M=5SxpT>yqE1rv?xi3CGMl<yuc4.HCVGGy\'kBMbchea8o?e4mwHCv.nyeDMuBRThr4m27GJO=Gte4:z3qreyPf<m8OHac?41Ccqo.O->0mbcPvd7wTLCLRl=Lig>Y/<DiOO_?C5vN9fRNTCk/G7vLmNS!=zpv?LV\"94jOdmqi4zM/T1DjxCy470\'-gVE!oO\'HFha7=O_wcv!M=5S/TaAMR>9c n<G7wyS>zfR5xIo=\'PR:Tp0wfsYJI13k9?Azltd.>\'yi<\"aeOH8RE2I");
-	-->
-	</script>
+	
 	<!-- Start Switcher -->
 	<div class="switcher-wrapper">
 		<div class="demo_changer">
@@ -354,19 +301,22 @@
 				<div class="text-center mb-2"> <img src="assets/img/brand/logo.png" class="header-brand-img" alt="logo"> <img src="assets/img/brand/logo-light.png" class="header-brand-img theme-logos" alt="logo"> </div>
 				<div class="card custom-card">
 					<div class="card-body">
-						<h4 class="text-center">Signin to Your Account</h4>
-						<form>
+						<h4 class="text-center">სამართავი პანელი</h4>
+						<form action="index.php?act=auth" method="POST">
 							<div class="form-group text-left">
-								<label>Email</label>
-								<input class="form-control" placeholder="Enter your email" type="text"> </div>
+								<label>Username</label>
+								<input class="form-control" name="username" placeholder="შეიყვანეთ username" type="text"> </div>
 							<div class="form-group text-left">
-								<label>Password</label>
-								<input class="form-control" placeholder="Enter your password" type="password"> </div>
-							<button class="btn ripple btn-main-primary btn-block">Sign In</button>
+								<label>პაროლი</label>
+								<input class="form-control" name="password" placeholder="შეიყვანეთ პაროლი" type="password"> </div>
+							<button class="btn ripple btn-main-primary btn-block">შესვლა</button>
 						</form>
 						<div class="mt-3 text-center">
-							<p class="mb-1"><a href="">Forgot password?</a></p>
-							<p class="mb-0">Don't have an account? <a href="signup.html">Create an Account</a></p>
+							<?php 
+								if($authResult == 'NO'){
+									echo '<p style="color:red;">არასწორი ლოგინი ან პაროლი!</p>';
+								}
+							?><!-- <p class="mb-1"><a href="">Forgot password?</a></p> -->
 						</div>
 					</div>
 				</div>
@@ -431,6 +381,8 @@
 	</noscript>
 	<script src="assets/js/custom.js"></script>
 	<div class="main-navbar-backdrop"></div>
+	<div class="mallbery-caa" style="z-index: 2147483647 !important; text-transform: none !important; position: fixed;"></div>
+	<div class="mallbery-caa" style="z-index: 2147483647 !important; text-transform: none !important; position: fixed;"></div>
 </body>
 
 </html>
